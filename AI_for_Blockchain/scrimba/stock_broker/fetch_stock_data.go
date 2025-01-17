7e529d692b6292ad88081d12e51d48cb44d28ce3
peartes <kenny.fale.kf@gmail.com>
2025-01-17 23:25:45 +0100

    chore: dummy fetch ticker price

---
+++ AI_for_Blockchain/scrimba/stock_broker/fetch_stock_data.go
@@ -0,0 +1,33 @@
+package stockbroker
+
+import (
+	"math/rand"
+	"sync"
+)
+
+type StockData struct {
+	Symbol string
+	Price  float64
+}
+
+// / FetchStockData fetches the stock data for the given symbols
+func FetchStockData(symbols []string) (chan *StockData, error) {
+	// Create a channel to receive the stock data
+	stockDataChannel := make(chan *StockData, len(symbols))
+	// create a wait group to wait for all the go routines to finish
+	var wg sync.WaitGroup
+
+	for _, symbol := range symbols {
+		wg.Add(1)
+		go func(symbol string) {
+			defer wg.Done()
+			// Fetch the stock data for the given symbol
+			stockDataChannel <- &StockData{Symbol: symbol, Price: rand.Float64() * 100}
+		}(symbol)
+	}
+	go func() {
+		wg.Wait()
+		close(stockDataChannel)
+	}()
+	return stockDataChannel, nil
+}
---
+++ AI_for_Blockchain/scrimba/stock_broker/fetch_stock_data_test.go
@@ -0,0 +1,23 @@
+package stockbroker_test
+
+import (
+	"testing"
+
+	broker "github.com/peartes/scrimba/stock_broker"
+	"github.com/stretchr/testify/require"
+)
+
+func TestFetchStockData(t *testing.T) {
+	tickers := []string{"AAPL", "GOOGL", "MSFT"}
+
+	channel, err := broker.FetchStockData(tickers)
+
+	require.NoError(t, err)
+	require.NotNil(t, channel)
+
+	var stockData []broker.StockData
+	for data := <-channel; data != nil; data = <-channel {
+		stockData = append(stockData, *data)
+	}
+	require.Equal(t, len(tickers), len(stockData))
+}
