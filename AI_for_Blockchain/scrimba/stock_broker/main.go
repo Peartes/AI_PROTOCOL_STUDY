dd179250591105b59f3f8285d95b858a4fcb17b7
peartes <kenny.fale.kf@gmail.com>
2025-01-23 10:17:06 +0100

    chore: make calls to chat completion endpoint using 4.0 snapshot

--- AI_for_Blockchain/scrimba/stock_broker/main.go
+++ AI_for_Blockchain/scrimba/stock_broker/main.go
@@ -1,16 +1,40 @@
 package main
 
 import (
+	"context"
+	"fmt"
+
 	openai "github.com/openai/openai-go"
 	openoptions "github.com/openai/openai-go/option"
+	"github.com/peartes/scrimba/stock_broker/config"
 )
 
 var client openai.Client
+var ctx context.Context
 
 func init() {
-	client = *openai.NewClient(openoptions.WithAPIKey(OpenAIKey), openoptions.WithOrganization(OpenAIOrganization), openoptions.WithProject(OpenAIProject))
+	client = *openai.NewClient(openoptions.WithAPIKey(config.GetOpenAIKey()), openoptions.WithOrganization(config.GetOpenAIOrganization()), openoptions.WithProject(config.GetOpenAIProject()))
+	ctx = context.Background()
+}
+
+func chatMessage(role openai.ChatCompletionMessageParamRole, content string) openai.ChatCompletionMessageParam {
+	return openai.ChatCompletionMessageParam{
+		Role:    openai.Raw[openai.ChatCompletionMessageParamRole](role),
+		Content: openai.Raw[interface{}](content),
+	}
 }
 
 func main() {
-
+	res, err := client.Chat.Completions.New(ctx, openai.ChatCompletionNewParams{
+		Model: openai.String(openai.ChatModelGPT4_0613),
+		Messages: openai.Raw[[]openai.ChatCompletionMessageParamUnion]([]openai.ChatCompletionMessageParamUnion{
+			chatMessage(openai.ChatCompletionMessageParamRoleSystem, "You are a helpful general knowledge expert."),
+			chatMessage(openai.ChatCompletionMessageParamRoleUser, "What is the capital of the United States?"),
+		}),
+	})
+	if err != nil {
+		fmt.Println(fmt.Errorf("error while asking model for chat completion %w ", err))
+	} else {
+		fmt.Printf("%s: %s \n", res.Choices[0].Message.Role, res.Choices[0].Message.Content)
+	}
 }
