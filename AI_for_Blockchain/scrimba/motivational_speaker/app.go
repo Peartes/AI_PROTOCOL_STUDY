de74282ec216516025921a5bf6a22a7885fe1924
peartes <kenny.fale.kf@gmail.com>
2025-02-01 16:15:37 +0100

    feat: properly create a fine tune jobs

--- AI_for_Blockchain/scrimba/motivational_speaker/app.go
+++ AI_for_Blockchain/scrimba/motivational_speaker/app.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"crypto/sha256"
+	"encoding/json"
 	"fmt"
 	"io"
 	"os"
@@ -34,70 +35,107 @@ func RunApp(filename string) error {
 	}
 	fmt.Println("File uploaded successfully with id: ", fd)
 
-	modelId, err := GetFineTunedModel(filename, openai.FineTuningJobNewParamsModelGPT3_5Turbo)
+	model, err := GetFineTunedModel(filename, openai.FineTuningJobNewParamsModelGPT3_5Turbo)
 	if err != nil {
 		return fmt.Errorf("error while checking if fine tuned model exists %w ", err)
 	}
-	fmt.Println("Fine tuned model already exists with id: ", modelId)
+	fmt.Printf("Model status: %s \nModel id: %s \nModel name: %s\n", model.JobStatus, model.ModelId, model.ModelName)
 	return nil
 }
 
 /*
 This function creates a fine tuned model with a particular doc
-Returns the (model id, error if any)
+Returns the (status, model id, model name, error if any)
 */
-func GetFineTunedModel(trainingFile string, model openai.FineTuningJobNewParamsModel) (string, error) {
+type FineTuneJob struct {
+	JobStatus string `json:"job_status"`
+	ModelId   string `json:"model_id"`
+	ModelName string `json:"model_name"`
+}
+
+func GetFineTunedModel(trainingFile string, model openai.FineTuningJobNewParamsModel) (*FineTuneJob, error) {
 	fd, err := os.Getwd()
 	if err != nil {
-		return "", fmt.Errorf("error while getting working directory %w ", err)
+		return nil, fmt.Errorf("error while getting working directory %w ", err)
 	}
 	file, err := os.OpenFile(path.Join(fd, "data", trainingFile), os.O_RDONLY, 0644)
 	if err != nil {
-		return "", fmt.Errorf("error while opening file %w ", err)
+		return nil, fmt.Errorf("error while opening file %w ", err)
 	}
 	defer file.Close()
 
 	checksum, err := createChecksum(file)
 	if err != nil {
-		return "", fmt.Errorf("error while creating checksum %w ", err)
+		return nil, fmt.Errorf("error while creating checksum %w ", err)
 	}
 
 	f, exist := db.Get(checksum, "fine_tuned_models")
 
 	if exist {
-		res, ok := (*f).(string)
-		if ok {
-			return res, nil
+		var res FineTuneJob
+		bz, err := json.Marshal(f)
+		if err != nil {
+			return nil, fmt.Errorf("error while marshalling fine tuned model from db %w ", err)
 		}
-		return "", fmt.Errorf("error while getting fine tuned model from db %w ", err)
+		err = json.Unmarshal(bz, &res)
+		if err != nil {
+			return nil, fmt.Errorf("error while un-marshalling fine tuned model from db %w ", err)
+		}
+		// check the status of the fine tuned model
+		if res.JobStatus == "succeeded" {
+			return &res, nil
+		} else if res.JobStatus != "failed" {
+			// model is not ready - check the status
+			job, err := client.FineTuning.Jobs.Get(ctx, res.ModelId)
+			if err != nil {
+				return nil, fmt.Errorf("error while getting fine tuned model status %w ", err)
+			}
+			t := FineTuneJob{
+				JobStatus: string(job.Status),
+				ModelId:   job.ID,
+				ModelName: job.FineTunedModel,
+			}
+			db.Set(checksum, &t, "fine_tuned_models")
+			return &t, nil
+		}
+		return &res, nil
 	} else {
 		fd, err := GetFileUploadFD(trainingFile)
 		if err != nil {
-			return "", fmt.Errorf("error while getting file upload descriptor %w ", err)
+			return nil, fmt.Errorf("error while getting file upload descriptor %w ", err)
 		}
-		modelId, err := CreateFineTunedModel(openai.FineTuningJobNewParams{
+		job, err := CreateFineTunedModel(openai.FineTuningJobNewParams{
 			Model:        openai.Raw[openai.FineTuningJobNewParamsModel](openai.FineTuningJobNewParamsModelGPT3_5Turbo),
 			TrainingFile: openai.String(fd),
 			Suffix:       openai.String("motivational_speaker"),
 		})
 		if err != nil {
-			return "", fmt.Errorf("error while creating a new fine tuned model %w ", err)
+			return nil, fmt.Errorf("error while creating a new fine tuned model %w ", err)
 		}
-		db.Set(checksum, modelId, "fine_tuned_models")
-		return modelId, nil
+		db.Set(checksum, job, "fine_tuned_models")
+
+		return job, nil
 	}
 }
 
 /*
 This function checks if there exists a finetuned model trained with a particular doc
-Returns the (model id, error if any)
+Returns the (FineTuneJob, model name, error if any)
+Returns the model name and id if the model is ready or the model id only if the model is not ready
 */
-func CreateFineTunedModel(opts openai.FineTuningJobNewParams) (string, error) {
+func CreateFineTunedModel(opts openai.FineTuningJobNewParams) (*FineTuneJob, error) {
 	fineTune, err := client.FineTuning.Jobs.New(ctx, opts)
 	if err != nil {
-		return "", fmt.Errorf("error while creating fine tuning job %w ", err)
+		return nil, fmt.Errorf("error while creating fine tuning job %w ", err)
 	}
-	return fineTune.ID, nil
+	if fineTune.Status == "failed" {
+		return nil, fmt.Errorf("error while fine tuning model %w ", err)
+	}
+	return &FineTuneJob{
+		JobStatus: string(fineTune.Status),
+		ModelId:   fineTune.ID,
+		ModelName: fineTune.FineTunedModel,
+	}, nil
 }
 
 /*
