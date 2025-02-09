b348d41b31d6a320d2c310ba29b178adeb3c79e5
peartes <kenny.fale.kf@gmail.com>
2025-02-09 22:34:32 +0100

    chore: add in hugging face translation and classification

--- AI_for_Blockchain/scrimba/hugging_face/text/app.go
+++ AI_for_Blockchain/scrimba/hugging_face/text/app.go
@@ -28,8 +28,31 @@ func TextGeneration() error {
 	return nil
 }
 
+func TextClassification() error {
+	res, err := client.TextClassification(ctx, &hf.TextClassificationRequest{
+		Inputs: "Today is a good day",
+	})
+	if err != nil {
+		return fmt.Errorf("error classifying text %w", err)
+	}
+	fmt.Printf("Classification is %s ", res[0][0].Label)
+	return nil
+}
+
+func TextTranslation(srcText, srcLanguage string) error {
+	res, err := client.Translation(ctx, &hf.TranslationRequest{
+		Inputs: []string{srcText},
+		Model:  "facebook/mbart-large-50-many-to-many-mmt",
+	})
+	if err != nil {
+		return fmt.Errorf("error classifying text %w", err)
+	}
+	fmt.Printf("translation is %s ", res[0].TranslationText)
+	return nil
+}
+
 func RunApp() error {
-	if err := TextGeneration(); err != nil {
+	if err := TextTranslation("Меня зовут Вольфганг и я живу в Берлине", "en_XX"); err != nil {
 		return fmt.Errorf("error running app: %w", err)
 	}
 	return nil
