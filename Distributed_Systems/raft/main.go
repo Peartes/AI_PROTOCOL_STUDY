76bfe359477324af9747260e84cf958c43bc0fa4
peartes <kenny.fale.kf@gmail.com>
2025-03-16 23:59:07 +0100

    chor: remove grpc server

--- Distributed_Systems/raft/main.go
+++ Distributed_Systems/raft/main.go
@@ -1,31 +1,4 @@
 package main
 
-import (
-	"log"
-	"net"
-
-	"github.com/peartes/distr_system/raft/rpc"
-	types "github.com/peartes/distr_system/raft/types"
-	"google.golang.org/grpc"
-)
-
 func main() {
-	// Create a listener on a TCP port
-	lis, err := net.Listen("tcp", ":50051")
-	if err != nil {
-		log.Fatalf("Failed to listen: %v", err)
-	}
-
-	// Create a new gRPC server
-	grpcServer := grpc.NewServer()
-
-	// Register the RaftService server
-	types.RegisterRaftServerServer(grpcServer, &rpc.RaftServer{})
-
-	log.Println("gRPC server listening on port 50051")
-
-	// Start serving
-	if err := grpcServer.Serve(lis); err != nil {
-		log.Fatalf("Failed to serve: %v", err)
-	}
 }
